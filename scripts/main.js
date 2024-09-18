cba0a8f3fe43465a1d530153765ee876e0085478
BlairHaze <126704689+BlairHaze@users.noreply.github.com>
2024-09-18 16:15:56 +0200

    Make form functional

--- scripts/main.js
+++ scripts/main.js
@@ -7,9 +7,16 @@ function Book(name, author, pages, status) {
     this.status = status;
 }
 
-function addBookToLibrary(book) {
-    myLibrary.push(book);
-    document.querySelector("#bookTable tbody").innerHTML = myLibrary.map(book => `<tr><td>${book.name}</td><td>${book.author}</td><td>${book.pages}</td><td>${book.status}</td></tr>`).join('');
+var myForm = document.forms.myForm;
+
+function openForm() {
+    document.getElementById("bookForm").style.display = "block";
+    document.getElementById("open-button").style.display = "none";
+}
+
+function closeForm() {
+    document.getElementById("bookForm").style.display = "none";
+    document.getElementById("open-button").style.display = "block";
 }
 
 const book1 = new Book("Harry Potter", "J.K. Rowling", "300", "read");
@@ -17,5 +24,18 @@ const book2 = new Book("Card Captor Sakura", "CLAMP", "50", "reading");
 
 addBookToLibrary(book1);
 addBookToLibrary(book2);
+
+myForm.onsubmit = function () {
+    const book = new Book(myForm.name.value, myForm.author.value, myForm.pages.value, myForm.status.value);
+    document.getElementById("bookForm").style.display = "none";
+    document.getElementById("open-button").style.display = "block";
+    addBookToLibrary(book);
+    return false;
+}
+
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+    document.querySelector("#bookTable tbody").innerHTML = myLibrary.map(book => `<tr><td>${book.name}</td><td>${book.author}</td><td>${book.pages}</td><td>${book.status}</td></tr>`).join('');
+}
 
 // document.querySelector("#bookTable tbody").innerHTML = myLibrary.map(name => `<tr><td>${book2.name}</td><td>${book2.author}</td><td>${book2.pages}</td><td>${book2.status}</td></tr>`).join('');
